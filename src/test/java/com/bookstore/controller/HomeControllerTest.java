bbfa0b81f41849c4970d348b3cb411b4ab3fb4c9
bromazepam <graor95@gmail.com>
2022-05-13 19:58:04 +0200

    Update HomeControllerTest.java

---
+++ src/test/java/com/bookstore/controller/HomeControllerTest.java
@@ -0,0 +1,75 @@
+package com.bookstore.controller;
+
+import com.bookstore.service.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+
+import static org.mockito.Mockito.reset;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ContextConfiguration(classes = HomeController.class)
+@ExtendWith(MockitoExtension.class)
+public class HomeControllerTest {
+
+    @InjectMocks
+    HomeController homeController;
+
+    @Mock
+    Model model;
+    @Mock
+    UserService userService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        reset(userService);
+    }
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+    }
+
+    @Test
+    public void testHours() throws Exception {
+        mockMvc.perform(get("/hours"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hours"));
+    }
+
+    @Test
+    public void testFAQ() throws Exception {
+        mockMvc.perform(get("/faq"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("faq"));
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        model.addAttribute("classActiveLogin");
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("classActiveLogin"))
+                .andExpect(view().name("myAccount"));
+    }
+
+}
